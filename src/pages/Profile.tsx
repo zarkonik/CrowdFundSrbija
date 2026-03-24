b43a0f2d5fe88fcd0e31356019fc9b1bddb3173a
zarkonik <zarko.nick@gmail.com>
2026-03-24 21:44:36 +0100

    Profile page izbacivanje React

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 // @ts-ignore
 import { DisplayCampaigns } from "../components";
 import { useStateContext } from "../context";
